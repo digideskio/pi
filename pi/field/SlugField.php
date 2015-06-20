71206de677e9e426696082a1e4eabc2d14dada1b
Yannick Armand (PifyZ) <yannick.armand@gmail.com>
2015-06-20 19:02:48 +0200

    Création des champs manquants : Color, Slug, Tags, Title et User

---
+++ pi/field/SlugField.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Pi\Field;
+
+use Pi\Lib\Html\Tag;
+
+class SlugField extends BaseField {
+	public $minLength;
+	public $maxLength;
+
+	public function __construct($data) {
+		parent::__construct($data);
+
+		$this->minLength = isset($data['minLength']) ? $data['minLength'] : 0;
+		$this->maxLength = isset($data['maxLength']) ? $data['maxLength'] : false;
+	}
+
+	public function html() {
+		$tag = new Tag('input', [
+			'name'  => $this->name,
+			'type'  => 'text',
+			'value' => $this->value()
+		]);
+
+		if ($this->required)
+			$tag->addAttr('required');
+
+		if ($this->placeholder)
+			$tag->addAttr('placeholder', $this->placeholder);
+
+		return $tag;
+	}
+}
