05ec642014e82ea1534bfd01c43009e71ff86232
Yannick Armand (PifyZ) <yannick.armand@gmail.com>
2016-03-29 18:37:27 +0200

    Constantes : utilisation du séparateur de dossiers

--- constants.php
+++ constants.php
@@ -21,10 +21,10 @@ const DS = DIRECTORY_SEPARATOR;
 const EOL = PHP_EOL;
 
 define('PI_DIR_SITE', dirname(__FILE__) . DS);
-define('PI_DIR_CONTENT', PI_DIR_SITE . 'content/');
-define('PI_DIR_MODELS', PI_DIR_CONTENT . 'models/');
-define('PI_DIR_PAGES', PI_DIR_CONTENT . 'pages/');
-define('PI_DIR_THEMES', PI_DIR_CONTENT . 'themes/');
+define('PI_DIR_CONTENT', PI_DIR_SITE . 'content' . DS);
+define('PI_DIR_MODELS', PI_DIR_CONTENT . 'models' . DS);
+define('PI_DIR_PAGES', PI_DIR_CONTENT . 'pages' . DS);
+define('PI_DIR_THEMES', PI_DIR_CONTENT . 'themes' . DS);
 
 define('PI_URL_SITE', $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/');
 define('PI_URL_CONTENT', PI_URL_SITE . 'content/');
