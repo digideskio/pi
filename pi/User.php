1097fc0b5c7cc4b6fa18e9d8e7a6471217025baa
Yannick Armand (PifyZ) <yannick.armand@gmail.com>
2016-03-29 22:13:25 +0200

    Complétion des configurations possibles : rôles et utilisateurs

---
+++ pi/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pi;
+
+// à faire
+class User {
+	protected $username;
+	protected $password;
+	protected $permissions;
+
+	// à faire
+	public function __construct() {
+		
+	}
+
+	// à faire
+	public function hasPermission($permission) {
+		return false;
+	}
+}
